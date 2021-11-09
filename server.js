fda2da86f0759e003e4735f91ddfbc87d427500b
D.L. White - Vunte Ron <dlwhite112@gmail.com>
2021-11-09 11:11:24 -0600

    deploying

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require("express");
+
+const mongoose = require("mongoose");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -18,6 +20,17 @@ if (process.env.NODE_ENV === "production") {
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
+
+// Connect to the Mongo DB
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/gbooks-dl',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
